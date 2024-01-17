e5409565bf7b303008d12349bf0dab67528ef09c
Wojciesh <wojciesh@gmail.com>
2024-01-17 03:10:52 +0100

    Add classes for monolithic quizapp

---
+++ src/main/java/com/wsh/questionservice/model/DifficultyLevel.java
@@ -0,0 +1,5 @@
+package com.wsh.questionservice.model;
+
+public enum DifficultyLevel {
+    EASY, MEDIUM, HARD
+}
---
+++ src/main/java/com/wsh/questionservice/model/QuestionWrapper.java
@@ -0,0 +1,41 @@
+package com.wsh.questionservice.model;
+
+import lombok.Data;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@Data
+@RequiredArgsConstructor
+public class QuestionWrapper {
+
+        @NonNull
+        private Integer id;
+
+        @NonNull
+        private String questionTitle;
+
+        @NonNull
+        private String option1;
+
+        @NonNull
+        private String option2;
+
+        @NonNull
+        private String option3;
+
+        @NonNull
+        private String option4;
+
+        public static List<QuestionWrapper> wrapQuestions(List<Question> questions) {
+                return questions.stream().map(
+                        question -> new QuestionWrapper(
+                                question.getId(),
+                                question.getQuestionTitle(),
+                                question.getOption1(),
+                                question.getOption2(),
+                                question.getOption3(),
+                                question.getOption4())).toList();
+        }
+}
